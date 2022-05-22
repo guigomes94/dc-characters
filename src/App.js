c17ea9143d8b917b5b3405c314f624a892e45fbf
guigomes94 <guigomes94@outlook.com>
2022-05-22 16:59:54 -0300

    update dependencies

--- src/App.js
+++ src/App.js
@@ -3,14 +3,14 @@ import './styles.css';
 
 import Navbar from './components/Navbar';
 import Header from './components/Header';
-import Routes from './routes';
+import MyRoutes from './routes';
 import Footer from './components/Footer';
 
 function App() {
   return (<div className="App">
     <Navbar />
     <Header />
-    <Routes />
+    <MyRoutes />
     <Footer />
   </div>
   )};
--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,16 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Main from './components/Main';
 import Character from './components/Info';
 
-export default function Routes() {
-    return (<BrowserRouter>
-        <Switch>
-            <Route exact path="/" component={Main} />
-            <Route path="/character/:id" component={Character} />
-        </Switch>
+export default function MyRoutes() {
+    return (
+    <BrowserRouter>
+        <Routes>
+            <Route exact path="/" element={<Main />} />
+            <Route path="/character/:id" element={<Character />} />
+        </Routes>
     </BrowserRouter>
-    )};
+    )
+};
 
